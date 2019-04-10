bfd13cd240f31beba41207b307a42213fc6ca5b9
youthke <47767005+youthke@users.noreply.github.com>
2019-04-10 21:57:04 +0900

    edit car_util

--- util/util.go
+++ util/util.go
@@ -10,3 +10,12 @@ func TotalPrice(books []model.Book) (int, error) {
 
 	return totalPrice, nil
 }
+
+func CarTotalPrice(cars []model.Car) (int, error) {
+	var car_total_price int
+	for _, car := range cars {
+		car_total_price += car.Price
+	}
+
+	return car_total_price, nil
+}
